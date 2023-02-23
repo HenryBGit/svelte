fb8421adc7fda8ecd4aa316b247ada256c8eae90
Henry Brookland (1000112868) <BROOHA1@student.op.ac.nz>
2023-02-24 11:25:31 +1300

    Added in a button that resets and each time the button resets you get a point

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,47 @@
-<h1>Welcome to my bloog</h1>
-<p>What are u doing here</p>
-<p>I see you</p>
+<style>
+
+button {
+    background-color: #e293b3;
+    color:white;
+}
+button:hover{
+    opacity: 0.7;
+}
+</style>
+<script>
+
+let count =0;
+let point =0;
+let reset =0;
+function increment_count() {
+    count++
+    button_reset()
+}
+
+function button_reset(){
+    if (count===6) {
+        count=0
+        reset++
+
+    }
+    points()
+
+}
+function points() {
+    
+        point++
+
+}
+</script>
+
+<button on:click={increment_count}>
+     
+<h1> Clicked {count} {count===1? "time": "times"}</h1>
+
+</button>
+
+<p>You have {reset} {reset===1? "point": "points"}</p>
+
+<body>
+
+</body>
