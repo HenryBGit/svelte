2e5fb54ef42bf8a365c98fc2cf803963d4b500fc
Henry Brookland (1000112868) <BROOHA1@student.op.ac.nz>
2023-02-28 09:56:38 +1300

    added some naviagtion to pages

---
+++ src/lib/navigation.svelte
@@ -0,0 +1,7 @@
+<nav>
+    <ul>
+        <li>1 <a href="/"> Homepage</a></li>
+        <li>2 <a href="/about"> About</a></li>
+        <li>3</li>
+    </ul>
+</nav>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+    import Navigation from "$lib/navigation.svelte";
+    import Footer from "$lib/footer.svelte"
+</script>
+<Navigation />
+<slot/>
+<br>
+And I am some common footer stuff 
+<Footer> </Footer>
