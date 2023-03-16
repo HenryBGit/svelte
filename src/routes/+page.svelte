a76709c9837b95eec0451864b1b9f0828cdf7a2e
Henry Brookland (1000112868) <BROOHA1@student.op.ac.nz>
2023-03-17 11:58:36 +1300

    change around some css and add images in a grid

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,49 +13,15 @@
     <img class="background_image" src="\images\Neoni home page image.jpg"  alt="">
 </div>
 <br>
-   <h1>Welcome To <a href="https://www.youtube.com/watch?v=oILljVDOkwY">Doomsday</a></h1>
-
-<!--For the main 6 square images-->
-    <!-- One -->
-        <section id="one" class="tiles">
-            <article>
-                <span class="image">
-                    <img src="images/pic01.jpg" alt="" />
-                </span>
-                <header class="major">
-                    <h3><a href="index.html" class="link">Home</a></h3>
-                    <p>This is the home page</p>
-                </header>
-            </article>
-            <article>
-                <span class="image">
-                    <img src="images/pic02.jpg" alt="" />
-                </span>
-                <header class="major">
-                    <h3><a href="Albums and Singles.html" class="link">Albums and Singles</a></h3>
-                    <p>Albums and Singles of Neoni</p>
-                </header>
-            </article>
-            <article>
-                <span class="image">
-                    <img src="images/pic03.jpg" alt="" />
-                </span>
-                <header class="major">
-                    <h3><a href="Original Soundtracks.html" class="link">About Neoni</a></h3>
-                    <p>About the song Artist</p>
-                </header>
-            </article>
-        </section>
-
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
+   <h1>Welcome To <a href="https://www.youtube.com/watch?v=lW9ep22YmlM">Royalty</a></h1>
+<h2>Fan Favourites</h2>
+<div class="container">
+    <div class="item-1"> <img src="\images\Royalty Album Cover.jpg"> <p>Royalty</p> </div>
+    <div class="item-2"> <img src="\images\Darkside Neoni.jpg"> <p>Darkside</p> </div>
+    <div class="item-3"> <img src="\images\Wonderland Neoni.jpg"> <p>Wonderland</p> </div>
+    <div class="item-4"> <img src="\images\Ghost Town Neoni.jpg"> <p>Ghost Town</p> </div>
+    <div class="item-5"> <img src = "\images\Loser Neoni.jpg"> <p>Loser</p> </div>
+</div>
    <footer>
     <ul>
         <li><a href="/"> Home</a> </li>
